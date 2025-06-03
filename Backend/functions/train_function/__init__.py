366e2ec5fe4a55702a8c9f9b6abeb8f6ff4242ec
Giuseppe Mattia Greco <giumatt99@gmail.com>
2025-06-03 13:45:39 +0200

    Update preprocess and train function

--- Backend/functions/train_function/__init__.py
+++ Backend/functions/train_function/__init__.py
@@ -1,39 +1,45 @@
 import azure.functions as func
-from azure.storage.blob import BlobServiceClient
-import pandas as pd
-from shared.preprocessing_utils import preprocess_dataset
-from shared.model_utils import save_model
-from sklearn.ensemble import RandomForestClassifier
+import logging
 import os
-from dotenv import load_dotenv
+import pandas as pd
+import io
+import joblib
+from azure.storage.blob import BlobServiceClient
+from shared.preprocessing_utils import preprocess
+from shared.model_utils import train_and_save_model
 
-load_dotenv()
+def main(inputblob: func.InputStream):
+    try:
+        logging.info(f"Trigger received: {inputblob.name}")
 
-def main(myblob: func.InputStream):
-    metadata = myblob.metadata or {}
-    wine_type = metadata.get('type', '').lower()
+        connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+        blob_service = BlobServiceClient.from_connection_string(connection_string)
 
-    if wine_type not in ['red', 'white']:
-        raise ValueError("Metadata 'type' missing or not valid. Use 'red' or 'white'.")
-    
-    df = pd.read_csv(myblob)
-    cleaned_df, scaler = preprocess_dataset(df)
+        filename = inputblob.name
+        if "red" in filename:
+            wine_type = "red"
+        elif "white" in filename:
+            wine_type = "white"
+        else:
+            logging.warning("Wine type not recognized")
+            return
+        
+        csv_bytes = inputblob.read()
+        df = pd.read_csv(io.BytesIO(csv_bytes))
 
-    connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        df_cleaned, scaler = preprocess(df)
+        cleaned_path = f"cleaned/cleaned_{wine_type}.csv"
+        blob_service.get_blob_client(container="cleaned", blob=cleaned_path)\
+                    .upload_blob(io.BytesIO(df_cleaned.to_csv(index=False).encode()), overwrite=True)
+        
+        model_bytes, scaler_bytes = train_and_save_model(df_cleaned, scaler)
 
-    cleaned_blob = blob_service_client.get_blob_client(container="cleaned", blob=f"{wine_type}_cleaned.csv")
-    cleaned_blob.upload_blob(cleaned_df.to_csv(index=False), overwrite=True)
-
-    model = RandomForestClassifier(
-        n_estimators=100,
-        random_state=42,
-        class_weight='balanced'
-    )
-    X = cleaned_df.drop("quality", axis=1)
-    y = cleaned_df["quality"]
-    model.fit(X, y)
-
-    model_buffer = save_model(model, scaler)
-    model_blob = blob_service_client.get_blob_client(container="models", blob=f"model_{wine_type}.pkl")
-    model_blob .upload_blob(model_buffer.getvalue(), overwrite=True)
+        blob_service.get_blob_client(container="models", blob=f"model_{wine_type}.pkl")\
+                    .upload_blob(io.BytesIO(model_bytes), overwrite=True)
+        blob_service.get_blob_client(container="models", blob=f"scaler_{wine_type}.pkl")\
+                    .upload_blob(io.BytesIO(scaler_bytes), overwrite=True)
+        
+        logging.info("Training and save completed")
+    except Exception as e:
+        logging.error(f"Error while training model: {e}")
+        
